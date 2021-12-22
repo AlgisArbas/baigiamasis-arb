adbf3dfed13381a579934951a23ecdaed25fbf11
AlgisArbas <arbaciauskas.a@gmail.com>
2021-12-22 14:57:35 +0200

    pataisyta be pavadinimo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,10 @@
 import Button from "./Button";
 
 function Navbar(){
-    return
-    <div className='mainNavigation'>
-        <div className='navbar'>
-            <Button name='Registration'/>
-            <Button name='Login'/>
-            <Button name='Products'/>
-        </div>
-        <h2>SKELBIU LT</h2>
+    return <div className='navbar'>
+        <Button name='Registration'/>
+        <Button name='Login'/>
+        <Button name='Products'/>
     </div>
 
 }
